a2d207150ad90689e39d0b88c9c3fe79898297c7
Sojib Hossain <sojibhossainbd75@gmail.com>
2025-02-25 16:42:07 +0600

    Mid term done

--- Phitron/Basic Data Structures/Module 12 - Mid Term/Palindrome.cpp
+++ Phitron/Basic Data Structures/Module 12 - Mid Term/Palindrome.cpp
@@ -6,16 +6,18 @@ class Node
 public:
     int val;
     Node *next;
+    Node *prev;
     Node(int val) // node with constructor
     {
         this->val = val;
         this->next = NULL;
+        this->prev = NULL;
     }
 };
 
 void insert_at_tail(Node *&head, Node *&tail, int val)
 {
-    Node *newNode = new Node(val); // create a new node
+    Node *newNode = new Node(val);
     if (head == NULL)
     {
         head = newNode;
@@ -23,19 +25,25 @@ void insert_at_tail(Node *&head, Node *&tail, int val)
         return;
     }
     tail->next = newNode;
+    newNode->prev = tail;
     tail = tail->next;
 }
 
-void print_linked_list(Node *head)
+bool is_palindrome(Node *&head, Node *&tail)
 {
-    Node *tmp = head;
-    int count = 0;
-    while (tmp != NULL)
+    Node *t_head = head;
+    Node *t_tail = tail;
+
+    while (t_head != NULL && t_tail != NULL)
     {
-        count++;
-        tmp = tmp->next;
+        if (t_head->val != t_tail->val)
+        {
+            return false;
+        }
+        t_head = t_head->next;
+        t_tail = t_tail->prev;
     }
-    cout << count << endl;
+    return true;
 }
 
 int main()
@@ -44,7 +52,7 @@ int main()
     Node *tail = NULL;
 
     int val;
-    while (true)
+    while (1)
     {
         cin >> val;
         if (val == -1)
@@ -53,7 +61,15 @@ int main()
         }
         insert_at_tail(head, tail, val);
     }
-    print_linked_list(head);
+
+    if (is_palindrome(head, tail))
+    {
+        cout << "YES";
+    }
+    else
+    {
+        cout << "NO";
+    }
 
     return 0;
 }
