208abae76fcf69a74e1a72e38595cceed59fcc0d
Sojib Hossain <sojibhossainbd75@gmail.com>
2025-04-09 02:49:15 +0600

    Module 13 complete

--- Phitron/Basic Data Structures/Module 13 - Stack Implementtation and STL/Stack_input_output.cpp
+++ Phitron/Basic Data Structures/Module 13 - Stack Implementtation and STL/Stack_input_output.cpp
@@ -31,25 +31,21 @@ public:
 int main()
 {
     myStack st;
-    st.push(10);
-    st.push(20);
-    st.push(30);
-    cout << st.top() << endl;
-    st.pop();
-    cout << st.top() << endl;
-    st.pop();
-    cout << st.top() << endl;
-    st.pop();
-    if (!st.empty())
+
+    int n;
+    cin >> n;
+    for (int i = 0; i < n; i++)
+    {
+        int x;
+        cin >> x;
+        st.push(x);
+    }
+
+    while (!st.empty())
     {
         cout << st.top() << endl;
-    }
-    if (!st.empty())
-    {
         st.pop();
     }
-    
-    
 
     return 0;
 }
