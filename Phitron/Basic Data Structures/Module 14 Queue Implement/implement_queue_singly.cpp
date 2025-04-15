a0bfcded0b9a4ed817a5ecadb0b4a0cb27aeda1e
Sojib Hossain <sojibhossainbd75@gmail.com>
2025-04-16 01:49:20 +0600

    Module 14.1 done

--- Phitron/Basic Data Structures/Module 14 Queue Implement/implement_queue_singly.cpp
+++ Phitron/Basic Data Structures/Module 14 Queue Implement/implement_queue_singly.cpp
@@ -12,35 +12,76 @@ class Node
         this->next = NULL;
     }
 };
-class Node
+
+class myQueue
 {
     public:
-        int val;
-        Node *next;
-    Node(int val) // node with constructor
-    {
-        this->val = val;
-        this->next = NULL;
-    }
-};
-class Node
-{
-    public:
-        int val;
-        Node *next;
-    Node(int val) // node with constructor
-    {
-        this->val = val;
-        this->next = NULL;
-    }
-};
+    Node* head = NULL;
+    Node* tail = NULL;
+    int sz = 0;
 
-kire koire
-
-দেক তো এখন ঠিক আছে নাকি
+    void push(int val)
+    {
+        sz++;
+        Node* newNode = new Node(val);
+        if(head == NULL)
+        {
+            head = newNode;
+            tail = newNode;
+            return;
+        }
+        tail->next = newNode;
+        tail = tail->next;
+    }
+    void pop()
+    {
+        sz--;
+        Node* deleteNode = head;
+        
+        head = head->next;
+        delete deleteNode;
+        if (head == NULL)
+        {
+            tail = NULL;
+        }
+    }
+    int front()
+    {
+        return head->val;
+    }
+    int back()
+    {
+        return tail->val;
+    }
+    int size()
+    {
+        return sz;
+    }
+    bool empty()
+    {
+        return head == NULL;
+        
+    }
+    
+};
 
 int main()
 {
+    myQueue q;
+    int n;
+    cin >> n;
+    for (int i = 0; i < n; i++)
+    {
+        int val;
+        cin >> val;
+        q.push(val);
+    }
+    while (!q.empty())
+    {
+        cout << q.front() << endl;
+        q.pop();
+    }
+    
     
 
 
