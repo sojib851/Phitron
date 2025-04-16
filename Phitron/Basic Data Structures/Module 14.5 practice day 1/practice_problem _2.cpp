ee728b1fe4eb217c416243739da7a2b9c5e8f8f4
Sojib Hossain <sojibhossainbd75@gmail.com>
2025-04-16 07:36:43 +0600

    Module 14.5 complete

--- Phitron/Basic Data Structures/Module 14.5 practice day 1/practice_problem _2.cpp
+++ Phitron/Basic Data Structures/Module 14.5 practice day 1/practice_problem _2.cpp
@@ -1,3 +1,9 @@
+/* Practice problem 2, Module 14-5
+Question: Take a stack of size N and a queue of size M as
+input. Then check if both of them are the same or not in the
+order of removing. You should use STL to solve this problem.
+*/
+
 #include <bits/stdc++.h>
 using namespace std;
 
--- Phitron/Basic Data Structures/Module 14.5 practice day 1/practice_problem_3.cpp
+++ Phitron/Basic Data Structures/Module 14.5 practice day 1/practice_problem_3.cpp
@@ -1,3 +1,18 @@
+/* Practice problem 3, Module 14-5
+
+Take a stack of size N as input and copy those elements
+to another stack to get the values in the order they were
+inserted and print them. You should use STL to solve this problem.
+
+
+Sample Input
+5
+10 20 30 40 50
+Sample Output
+10 20 30 40 50
+
+*/
+
 #include <bits/stdc++.h>
 using namespace std;
 
